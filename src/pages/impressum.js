3ab1db4e5a9bc28f91ebb7403117ca2c978ad215
Markus Günther <markus.guenther@noerdisch.de>
2018-11-19 20:10:53 +0100

    New: Create new pages for imprint and blog

---
+++ src/pages/impressum.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const Imprint = () => (
+  <Layout>
+    <h1>Impressum</h1>
+    <p>Welcome </p>
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
+
+export default Imprint

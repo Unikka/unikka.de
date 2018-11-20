4199cce46396918ae6a1426e13cfed7ed59a9856
Markus Günther <markus.guenther@noerdisch.de>
2018-11-20 10:40:54 +0100

    New: Add typography.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,12 @@ module.exports = {
           }
       }
     },
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      }
+    },
     'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-source-filesystem`,

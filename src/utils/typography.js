d662264b527940e3aeb56316d82618d9d2e93ebb
Markus Günther <markus.guenther@noerdisch.de>
2018-11-20 14:47:20 +0100

    New: Add bootstrap theme for typography

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,17 +1,6 @@
 import Typography from "typography"
+import theme from 'typography-theme-bootstrap'
 
-const typography = new Typography({
-  baseFontSize: "20px",
-  baseLineHeight: 1.666,
-  headerFontFamily: [
-    "Segoe UI",
-    "Avenir Next",
-    "Helvetica Neue",
-    "Helvetica",
-    "Arial",
-    "sans-serif",
-  ],
-  bodyFontFamily: ["Segoe UI", "Georgia", "serif"],
-})
+const typography = new Typography(theme)
 
 export default typography

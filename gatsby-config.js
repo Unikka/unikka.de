7ba76336d8fdb975470402296e262ab720e4cc16
Markus Günther <markus.guenther@noerdisch.de>
2018-11-23 19:45:51 +0100

    New: Add postcss with nesting
    Also update the manifest package and define unikka logo file
    and branding.

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,7 @@ module.exports = {
     title: 'Gatsby Default Starter',
   },
   plugins: [
+    `gatsby-plugin-postcss`,
     {
       resolve: 'gatsby-plugin-react-svg',
       options: {
@@ -31,13 +32,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'Unikka',
+        short_name: 'unikka',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#89C53F',
+        theme_color: '#89C53F',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/unikka-icon.png', // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality

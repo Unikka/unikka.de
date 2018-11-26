a054bde2ad1b859d560ce71efc00753e8f25ff39
Markus Günther <markus.guenther@noerdisch.de>
2018-11-26 22:58:52 +0100

    New: Add form row component

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -2,10 +2,12 @@ import Form from './form'
 import InputField from './inputField'
 import TextArea from './textArea'
 import Submit from './submit'
+import Row from './row'
 
 export {
   Form,
   InputField,
   TextArea,
-  Submit
+  Submit,
+  Row
 }
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Layout from '../components/layout'
-import { Form, InputField, TextArea, Submit } from '../components/Form/'
+import { Form, InputField, TextArea, Submit, Row } from '../components/Form/'
 import { faPaperPlane } from '@fortawesome/free-regular-svg-icons'
 
 const Contact = () => {
@@ -10,9 +10,11 @@ const Contact = () => {
       <p>Wir würden uns freuen, von Ihnen zu hören. Schreiben Sie uns, wenn Sie Fragen zu unseren Projekten oder Dienstleistungen haben.</p>
 
       <Form name='contact'>
-        <InputField name='name' label='Name' />
-        <InputField name='email' label='E-Mail' type='email'/>
-        <TextArea name='message' label='Nachricht' rows='5' />
+        <Row>
+          <InputField name='name' label='Name' />
+          <InputField name='email' label='E-Mail' type='email'/>
+        </Row>
+        <TextArea name='message' label='Nachricht' rows='8' />
         <Submit label='Nachricht senden' icon={faPaperPlane} />
       </Form>
     </Layout>

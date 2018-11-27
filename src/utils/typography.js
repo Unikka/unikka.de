215342062fda39d2ff5a431ab4b7d4c48f49c36f
Markus Günther <markus.guenther@noerdisch.de>
2018-11-27 20:28:51 +0100

    Fix: Disable typography js

--- src/utils/typography.js
+++
@@ -1,7 +0,0 @@
-import Typography from "typography"
-import theme from 'typography-theme-bootstrap'
-
-theme.bodyFontFamily = ['Roboto', 'Droid Sans', 'Helvetica Neue', 'sans-serif'];
-const typography = new Typography(theme);
-
-export default typography

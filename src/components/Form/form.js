2ae80036b742f4d7d76b67018749d07bdb70c865
markusguenther <mukguenther@gmail.com>
2019-01-10 11:44:18 +0100

    Bugfix: Add hidden form field for form-name
    Gatsby strips out input fields that are not included in the JSX form, so you will still need to add the form-name hidden input field as described in.

--- src/components/Form/form.js
+++ src/components/Form/form.js
@@ -3,7 +3,15 @@ import formStyles from '../../css/forms.module.css'
 
 const Form = ({name, children}) => {
   return (
-    <form action="/contact/success" name={name} method="POST" className={formStyles.form} data-netlify="true" netlify-honeypot="more-infos">
+    <form
+      action="/contact/success"
+      name={name}
+      method="POST"
+      className={formStyles.form}
+      data-netlify="true"
+      netlify-honeypot="more-infos"
+    >
+      <input type="hidden" name="form-name" value={name} />
       <input name="more-infos" />
       {children}
     </form>

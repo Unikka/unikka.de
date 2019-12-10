d492dd13ee22ff1f0ea4840805ca84731165d42f
Markus Günther <info@unikka.de>
2019-12-10 16:12:33 +0100

    New: Replace netlify forms with formspree

--- src/components/Form/form.js
+++ src/components/Form/form.js
@@ -1,18 +1,49 @@
 import React from 'react'
+import { navigate } from 'gatsby'
 import formStyles from '../../css/forms.module.css'
 
-const Form = ({name, children}) => {
+const Form = ({ name, children }) => {
+  const submitForm = async event => {
+    event.preventDefault()
+    const form = event.target
+    const data = getFormData(form)
+
+    try {
+      const response = await fetch(form.action, {
+        method: form.method,
+        body: JSON.stringify(data),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      const json = await response.json()
+      if (json.ok === true) {
+        navigate('contact/success')
+      }
+    } catch (error) {
+      console.error('Error:', error)
+    }
+  }
+
+  const getFormData = form => {
+    const data = new FormData(form)
+    const formData = {}
+    data.forEach((value, key) => {
+      formData[key] = value
+    })
+
+    return formData
+  }
+
   return (
     <form
-      action="/contact/success"
+      action="https://formspree.io/mwkpzklw"
       name={name}
       method="POST"
       className={formStyles.form}
-      data-netlify="true"
-      netlify-honeypot="more-infos"
+      onSubmit={submitForm}
     >
       <input type="hidden" name="form-name" value={name} />
-      <input name="more-infos" />
       {children}
     </form>
   )

452e012227bdd47992097c3b1ae0d8079f13715d
Markus Günther <info@unikka.de>
2020-01-21 10:10:13 +0100

    New: Adding consulting to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,24 +2,33 @@ import React from 'react'
 import Layout from '../components/layout'
 import { FeatureBoxItem, FeatureBoxContainer } from '../components/FeatureBox/'
 import { faNeos, faReact, faPhp } from '@fortawesome/free-brands-svg-icons'
+import { faUserTie } from '@fortawesome/free-solid-svg-icons'
 
 const IndexPage = () => (
-  <Layout showHeaderTeaser={true} >
+  <Layout showHeaderTeaser={true}>
     <section>
       <h1>Was wir anbieten</h1>
       <FeatureBoxContainer>
         <FeatureBoxItem
-            title='Mobile Apps'
-            description='Crossplattform Applikationen auf Basis von React-Native. '
-            icon={faReact} />
+          title="Webseiten"
+          description="Internetpräsenzen auf Basis des Open-Source CMS Neos."
+          icon={faNeos}
+        />
         <FeatureBoxItem
-          title='Webseiten'
-          description='Internetpräsenzen auf Basis des Open-Source CMS Neos.'
-          icon={faNeos} />
+          title="Web Applikationen"
+          description="Individuelle Umsetzung einer Webanwendung."
+          icon={faPhp}
+        />
         <FeatureBoxItem
-          title='Web Applikationen'
-          description='Individuelle Umsetzung einer Webanwendung.'
-          icon={faPhp} />
+          title="Beratung"
+          description="Individuelle Beratung bei Fragen zu Neos, Web-Applikationen, Accessibility und Digitalisierung."
+          icon={faUserTie}
+        />
+        <FeatureBoxItem
+          title="Mobile Apps"
+          description="Crossplattform Applikationen auf Basis von React-Native. "
+          icon={faReact}
+        />
       </FeatureBoxContainer>
     </section>
   </Layout>
